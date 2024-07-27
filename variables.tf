1b31e410167ecd8ae96d3617349263788850eef3
paula1araujo <paula1_araujo@hotmail.com>
2024-07-27 06:39:26 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "admin_password" {
+  description = "The admin password for the VM"
+  type        = string
+}
+
+variable "ssh_key" {
+  description = "SSH key for VM access"
+  type        = string
+}
