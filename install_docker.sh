31c7ab2519cee674c1bb70ee4d66f6bbe7d0cd91
paula1araujo <paula1_araujo@hotmail.com>
2024-07-27 06:49:31 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -16,6 +16,35 @@ sudo usermod -aG docker ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Create Docker Compose file
+cat <<EOF > /home/${USER}/docker-compose.yml
+version: '3.1'
+
+services:
+  wordpress:
+    image: wordpress:latest
+    ports:
+      - "80:80"
+    environment:
+      WORDPRESS_DB_HOST: db
+      WORDPRESS_DB_USER: root
+      WORDPRESS_DB_PASSWORD: AfricaTwin01122024
+      WORDPRESS_DB_NAME: wordpress
+    volumes:
+      - wordpress_data:/var/www/html
+
+  db:
+    image: mysql:5.7
+    environment:
+      MYSQL_ROOT_PASSWORD: AfricaTwin01122024
+      MYSQL_DATABASE: wordpress
+    volumes:
+      - db_data:/var/lib/mysql
+
+volumes:
+  wordpress_data:
+  db_data:
+EOF
+
 # Run Docker Compose to set up WordPress and MySQL containers
-cd /home/${USER}
-docker-compose up -d
+docker-compose -f /home/${USER}/docker-compose.yml up -d
